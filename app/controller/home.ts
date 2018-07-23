c52d58ca69ee65ef1a1dd0f60b4db54d26fa7108
江画 <qijun.qj@alibaba-inc.com>
2018-07-23 20:46:43 +0800

    init

---
+++ app/controller/home.ts
@@ -0,0 +1,10 @@
+import { Controller } from 'egg';
+
+export default class HomeController extends Controller {
+  public async index() {
+    const { ctx } = this;
+    ctx.body = {
+      test:'hello world'
+    }
+  }
+}
